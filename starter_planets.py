f7bea8dc5758c9e3d8fa8c9c4f46ebe057cc4d15
THEBEST7192 <123459429+THEBEST7192@users.noreply.github.com>
2026-04-03 15:03:11 +0200

    feat(nuke): Add Y axis to planet object to support elliptical orbit

--- starter_planets.py
+++ starter_planets.py
@@ -30,30 +30,30 @@ class SolarSystem(Window):
                           batch=self.batch)
 
         # Planets (EDIT THESE!)
-        # (distance, speed, size, color)
+        # (x_distance, y_distance, speed, size, color)
         self.planets = [
-            (80, 1.0, 8, "#00D2FC"),   # Planet 1
-            (80, 1.0, 8, "#00D2FC"),  # Planet 2
-            (140, 0.5, 10, "#FFC75F"), # Planet 3
-            (200, 0.2, 12, "#C34A36"), # Planet 4
+            (80, 80, 1.0, 8, "#00D2FC"),   # Planet 1
+            (80, 80, 1.0, 8, "#00D2FC"),  # Planet 2
+            (140, 140, 0.5, 10, "#FFC75F"), # Planet 3
+            (200, 200, 0.2, 12, "#C34A36"), # Planet 4
 
             # Nucelear Reactor 
-            (100, 1000.0, 5, "#FFFFFF"),  # Core 1
-            (120, 1000.0, 5, "#FFFFFF"),  # Core 2
-            (140, 1000.0, 5, "#FFFFFF"), # Core 3
-            (160, 1000.0, 5, "#FFFFFF"),  # Core 4
-            (180, 1000.0, 5, "#FFFFFF"),  # Core 5
-            (200, 1000.0, 5, "#FFFFFF") # Core 6 
+            (100, 100, 1000.0, 5, "#FFFFFF"),  # Core 1
+            (120, 120, 1000.0, 5, "#FFFFFF"),  # Core 2
+            (140, 140, 1000.0, 5, "#FFFFFF"), # Core 3
+            (160, 160, 1000.0, 5, "#FFFFFF"),  # Core 4
+            (180, 180, 1000.0, 5, "#FFFFFF"),  # Core 5
+            (200, 200, 1000.0, 5, "#FFFFFF") # Core 6 
         ]
 
         # Create planet objects
         self.planet_circles = []
-        for distance, speed, size, color in self.planets:
-            circle = Circle(self.cx + distance, self.cy,
+        for x_distance, y_distance, speed, size, color in self.planets:
+            circle = Circle(self.cx + x_distance, self.cy + y_distance,
                             size,
                             color=hex_to_rgb(color),
                             batch=self.batch)
-            self.planet_circles.append((distance, speed, circle))
+            self.planet_circles.append((x_distance, y_distance, speed, circle))
 
         self.angle = 0
 
@@ -61,9 +61,9 @@ class SolarSystem(Window):
         self.angle += dt
 
         # Move planets in circles
-        for distance, speed, circle in self.planet_circles:
-            circle.x = self.cx + distance * math.cos(self.angle * speed)
-            circle.y = self.cy + distance * math.sin(self.angle * speed)
+        for x_distance, y_distance, speed, circle in self.planet_circles:
+            circle.x = self.cx + x_distance * math.cos(self.angle * speed)
+            circle.y = self.cy + y_distance * math.sin(self.angle * speed)
 
     def on_draw(self):
         self.clear()
